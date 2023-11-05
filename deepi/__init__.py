dd7afe7477c0ecb13ae39296b3b263ad669660c0
Russell Shomberg <717494+rshom@users.noreply.github.com>
2023-11-05 15:44:40 -0500

    update

--- deepi/__init__.py
+++ deepi/__init__.py
@@ -3,7 +3,7 @@ The DEEPi-python package provides functionality for field deploying
 DEEPi and similar Raspberry Pi based cameras.
 '''
 # from .camconfig import *
-from .webapp import *
+# from .webapp import *
 from .lights import *
 from .camera import *
 from .webstreaming import *
